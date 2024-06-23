abf366d56b09db0ab0ce2ec56f49818867768502
Kosbys <kosmicabysslord@protonmail.com>
2024-06-24 00:23:07 +0300

    Pages branch

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Main from "./Components/Main";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Main></Main>
-  </React.StrictMode>
-);
